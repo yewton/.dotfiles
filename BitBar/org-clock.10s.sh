d53b8ac011e7e0d8e5998467aa2f59c7a4c4e892
yewton <yewton@gmail.com>
2020-08-02 08:53:51 +0900

    ✨ clock-in しているタスクのトータル消費時間ではなく、現在の消費時間を出力する

--- BitBar/org-clock.10s.sh
+++ BitBar/org-clock.10s.sh
@@ -7,9 +7,11 @@
     (condition-case nil
       (server-eval-at \"server\"
         '(if (org-clocking-p)
-           (let ((len 50)
-                 (ellipsis \"...\")
-                 (s (format \"[%s]%s\" (org-duration-from-minutes (org-clock-get-clocked-time)) org-clock-heading)))
+           (let* ((len 50)
+                  (ellipsis \"...\")
+                  (currently-clocked-duration (org-duration-from-minutes
+                    (floor (org-time-convert-to-integer (org-time-since org-clock-start-time)) 60)))
+                  (s (format \"[%s]%s\" currently-clocked-duration org-clock-heading)))
              (format \":alarm_clock:%s\"
                      (if (> (length s) len)
                          (format \"%s%s\" (substring s 0 (- len (length ellipsis))) ellipsis)
